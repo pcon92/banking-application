f3ca02cfad77d12f6d609072ced829da12616166
Paul C <paul_congiu@hotmail.com>
2021-06-10 10:26:42 +1000

    added basic email and password storage in local nedb database

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+
 const express = require('express');
 const app = express();
-const router = express.Router();
-const path = require("path");
+const Datastore = require('nedb');
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => {console.log(`Listening on Port ${PORT}`)})
 
 app.use(express.static('public'));
+app.use(express.json({limit: '10mb'}));
 
-const PORT = process.env.port || 3000;
-app.listen(PORT, () => {console.log(`Listening on Port ${PORT}`)})
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/register-user', (req, res) => {
+    database.insert({
+        email: req.body.email,
+        password: req.body.password
+    })
+    res.json({
+        result: 'User added to database'
+    });
+});
--- src/components/register.js
+++ src/components/register.js
@@ -5,6 +5,31 @@ import styles from '../styles/register.module.css';
 
 const Register = () => {
 
+   const registerUser = async () => {
+
+       const EMAIL = document.getElementById('email');
+       const PASSWORD = document.getElementById('password');
+       const CONFIRM_PASSWORD = document.getElementById('confirm-password');
+    
+       const userDetails = {
+            email: EMAIL.value, 
+            password: PASSWORD.value
+        };
+
+        const fetchOptions = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userDetails)
+        }
+
+        const response = await fetch('/register-user', fetchOptions);
+        const json = await response.json();
+        console.log(json);
+
+    };
+
     return (
         <>
             <div className={`${styles.background}`}>
@@ -12,18 +37,20 @@ const Register = () => {
                     <h1 className={`${styles.heading}`}>Create an Account</h1>
                     <div className={`${styles.innerContainer}`}>
                         <div className={`${styles.emailAndPasswordDiv}`}>
-                            <label for="email">Email Address: </label>
+                            <label htmlFor="email">Email Address: </label>
                             <input type="text" id="email"/>
                         </div>
                         <div className={`${styles.emailAndPasswordDiv}`}>
-                            <label for="password">Password: </label>
+                            <label htmlFor="password">Password: </label>
                             <input type="password" id="password"/>
                         </div>
                         <div className={`${styles.emailAndPasswordDiv}`}>
-                            <label for="confirm-password">Confirm Password: </label>
+                            <label htmlFor="confirm-password">Confirm Password: </label>
                             <input type="password" id="confirm-password"/>
                         </div>
-                        <Link to="/confirm-registration-page" className={`${styles.registerButton}`}>Register Account</Link>
+                        <Link to="/confirm-registration-page" 
+                        className={`${styles.registerButton}`}
+                        onClick={registerUser}>Register Account</Link>
                     </div>
                     <p className={`${styles.loginText}`}>Already have an account?
                         <Link to="/login" className={`${styles.loginLink}`} >Login</Link>
