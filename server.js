fea4e8e8cb91929c49554bc85099a45562927345
Paul C <paul_congiu@hotmail.com>
2021-07-02 12:47:18 +1000

    deployment to heroku try 7

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 
 
-const PORT = process.env.port || 5000;
-app.listen(PORT, () => {console.log(`Listening on Port ${PORT}`)})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {console.log(`Listening on Port ${port}`)})
 
 app.use(express.json({limit: '10mb'}));
 
